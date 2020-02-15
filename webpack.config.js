9d9f09976332b1787a38dfd49b8f49ba1454a852
natalie <natalie@enkin.co.il>
2020-02-15 11:29:28 +0200

    setting frontend app as a react app in django

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports={
+    module:{
+        rules: [
+            {
+                // reggular espression: for any js
+                test:/\.js$/,
+                exclude: /node_modules/,
+                use:{
+                    // use babel to transpile the code
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+}
