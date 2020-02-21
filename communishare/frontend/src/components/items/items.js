79e1cdaa31d44ef5b59b51bdbaa61dc201048945
natalie <natalie@enkin.co.il>
2020-02-21 10:33:05 +0200

    adding alittle stype to items dashboard

--- communishare/frontend/src/components/items/items.js
+++ communishare/frontend/src/components/items/items.js
@@ -1,19 +1,21 @@
 import React, {Component, useState, useEffect} from 'react'
-import {get_all_items, get_current_user} from '../api_calls'
+
 
 export default function Items(props) {
     const {last_10} = props
-    const item_view = (item)  => (
-        <li key={item.name}>
-            <h2>{item.name}</h2>
-            <div>{item.description}</div>
-            {item.images.map(img => (<img src={img}/>))}
-
-        </li>
+    const item_view = (item) => (
+        <div className="card col-3" key={item.name}>
+            {item.images.length>0 ? item.images.map(img => (<img src={img} className="card-img-top" alt="Card image cap"/>)) :
+                <div className="no_image card-img-top card-img text-center">No image available</div>}
+                <div className="card-body">
+                    <h5 className="card-title">{item.name}</h5>
+                    <p className="card-text text-truncate">{item.description}</p>
+                    {item.available ? <a href="#" className="btn btn-warning">In use</a> :
+                        <a href="#" className="btn btn-primary">Borrow</a>}
+                </div>
+        </div>
     );
-    return (<ul>
+    return (<div className='row'>
         {last_10.map(item => item_view(item))}
-
-    </ul>);
-
+    </div>);
 };
